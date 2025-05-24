9fa8c8b4a91cf431b9e0fc0c8bee99ebd8269941
Thomas Retornaz <thomas.retornaz@mines-paris.org>
2025-05-24 12:54:48 +0200

    [SIMD] disable fpu=neon for macos

--- cmake/SimdDefaultFlags.cmake
+++ cmake/SimdDefaultFlags.cmake
@@ -25,3 +25,7 @@ elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^(aarch64.*|AARCH64.*|arm64.*|ARM64.*|arm
     endif()
 endif()
 
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
+    message(STATUS "Removing '-mfpu' flag for macOS arm64")
+    string(REPLACE "-mfpu=" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+endif()
