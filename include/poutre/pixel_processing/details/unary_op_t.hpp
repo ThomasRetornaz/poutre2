4d195878ae3ae1d67237a98d0603d466e32fc382
Thomas Retornaz <thomas.retornaz@mines-paris.org>
2025-06-04 06:51:51 +0200

    [PixelProcessing] play with threads and missing headers

--- include/poutre/pixel_processing/details/unary_op_t.hpp
+++ include/poutre/pixel_processing/details/unary_op_t.hpp
@@ -19,10 +19,12 @@
  *
  */
 #include <algorithm>//transform
+#include <iostream>
 #include <poutre/base/config.hpp>
-#include <poutre/base/details/simd/simd_algorithm.hpp> //simd transform
 #include <poutre/base/details/data_structures/array_view.hpp>
 #include <poutre/base/details/data_structures/image_t.hpp>
+#include <poutre/base/details/simd/simd_algorithm.hpp>//simd transform
+#include <poutre/base/details/threading/threading_helpers.hpp>//thread pool
 
 namespace poutre::details {
 
@@ -122,33 +124,45 @@ struct PixelWiseUnaryOpDispatcher<TIn,
     auto i_vinbeg = i_vin.data();
     auto i_vinend = i_vin.data() + i_vin.size();
     auto i_voutbeg = o_vout.data();
-    /*
-    const auto length    = i_vin.size();
-    if( !length )
-      return;
 
-    const auto block_size = 25 * poutre::TypeTraits<TIn>::simd_loop_step; // FIXME make this configurable
-    const auto                                    num_blocks = (length + block_size - 1) / block_size;
-    std::vector<poutre::thread::TaskFuture<TOut*>> futures(num_blocks - 1);
-    poutre::thread::TreadPool                     pool {poutre::thread::POUTRE_NUM_THREADS - 1};
+    const auto length    = i_vin.size();
+    if( !length ) {
+      return;
+    }
+    simd::transform(i_vinbeg, i_vinend, i_voutbeg, op);
+    // work but speedup is only 20% for 1024 images and degrade for smaller size
+    /* const auto block_size = 2048 * poutre::TypeTraits<TIn>::simd_loop_step;
+    const auto num_blocks = (length + block_size - 1) / block_size;
+    if (num_blocks<=1) {
+      simd::transform(i_vinbeg, i_vinend, i_voutbeg, op);
+      return;
+    }
+    std::vector<poutre::thread::TaskFuture<TOut*>> futures;
+    auto pool = poutre::thread::TreadPool(num_blocks);
 
     TIn * block_start    = i_vinbeg;
     TOut *block_startout = i_voutbeg;
-    for( size_t i = 0; i < (num_blocks - 1); ++i ) // FIXME make this configurable
+
+    TIn *block_end = nullptr;
+    TOut *block_out_end = nullptr;
+
+    for( size_t i = 0; i < (num_blocks - 1); ++i )
     {
-      TIn *block_end = block_start;
+      block_end = block_start;
+      block_out_end = block_startout;
       std::advance(block_end, block_size);
-      futures[i]=pool.submit([&] {return simd::transform(block_start, block_end, i_voutbeg, op); });
+      std::advance(block_out_end, block_size);
+      futures.push_back(pool.submit([=] {return simd::transform(block_start, block_end, block_startout, op); }));
       block_start = block_end;
-      std::advance(block_startout, block_size);
+      block_startout = block_out_end;
+      //std::advance(block_startout, block_size);
     }
-    simd::transform(block_start, i_vinend, block_startout, op);
+    simd::transform(block_start, i_vinend, block_startout, op);// epilogue last part
     for( auto &f : futures )
     {
       f.get();
     }
     */
-    simd::transform(i_vinbeg, i_vinend, i_voutbeg, op);
   }
 };
 
