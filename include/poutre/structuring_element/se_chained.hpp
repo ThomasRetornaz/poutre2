ceeca31cdfcf037818f0afdfff8b7297176148d9
Thomas Retornaz <thomas.retornaz@mines-paris.org>
2025-06-10 15:09:44 +0200

    [SE] add chained interface

---
+++ include/poutre/structuring_element/se_chained.hpp
@@ -0,0 +1,83 @@
+
+//==============================================================================
+//                  Copyright (c) 2015 - Thomas Retornaz                      //
+//                     thomas.retornaz@mines-paris.org                        //
+//          Distributed under the Boost Software License, Version 1.0.        //
+//                 See accompanying file LICENSE.txt or copy at               //
+//                     http://www.boost.org/LICENSE_1_0.txt                   //
+//==============================================================================
+
+#pragma once
+
+/**
+ * @file   se_chained.hpp
+ * @author Thomas Retornaz
+ * @brief  Generic interface for chained list of SE
+ *
+ *
+ */
+
+#include <poutre/base/config.hpp>
+#include <poutre/structuring_element/structuring_element.hpp>
+#include <poutre/structuring_element/se_interface.hpp>
+#include <vector>
+#include <memory>
+
+namespace poutre::se {
+/**
+ * @addtogroup poutre_se_chained_group Chain of SE, usefull to chain neighbor list se after a decomposition of a larger one
+ * * @ingroup se_group
+ *@{
+ */
+
+class SE_API ChainedStructuringElement : public IStructuringElement {
+
+public:
+  ~ChainedStructuringElement() override = default;
+
+  //! Append the se to the list @WARNING the ownership is transferred
+  void append(std::unique_ptr<IStructuringElement> strel);
+
+  /*!
+   *@name  Virtual methods inherited from IStructuringElement
+   *@{
+   */
+
+  [[nodiscard]] se_type GetType() const override {
+    return se_type::chain;
+  }
+
+  //! Returns a new structuring element that is a transposed copy
+  [[nodiscard]] std::unique_ptr<IStructuringElement> Transpose() const override;
+
+  /*!@brief Removes the center point from the structuring element
+   * @warning Not implemented
+   * @throw runtime_error
+   */
+  [[nodiscard]] std::unique_ptr<IStructuringElement> RemoveCenter() const override;
+
+  /*!@brief Returns the size of the structuring element in number of neighbors (the center, if defined, is included)
+   * @warning return 0
+   */
+  [[nodiscard]] std::size_t GetSize() const override;
+
+  //! Returns a deep copy of this structuring element
+  [[nodiscard]] std::unique_ptr<IStructuringElement> Clone() const override;
+
+  /*!@brief Checks the equality this structuring element with the provided one
+   * @warning tests equality item by item
+   */
+  [[nodiscard]] bool is_equal(const IStructuringElement& ) const noexcept override;
+
+  /*! Checks the equality of the structuring element with the argument, but without considering any ordering
+  * @warning tests equality item by item
+  */
+  [[nodiscard]] bool is_equal_unordered(const IStructuringElement& ) const noexcept override;
+
+  //! @}
+private:
+  std::vector<std::unique_ptr<IStructuringElement>> m_vect_se;
+};
+
+//! @} doxygroup: poutre_se_chained_group
+}// namespace poutre
---
+++ src/structuring_element/se_chained.cpp
@@ -0,0 +1,87 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+//==============================================================================
+//                  Copyright (c) 2015 - Thomas Retornaz                      //
+//                     thomas.retornaz@mines-paris.org                        //
+//          Distributed under the Boost Software License, Version 1.0.        //
+//                 See accompanying file LICENSE.txt or copy at               //
+//                     http://www.boost.org/LICENSE_1_0.txt                   //
+//==============================================================================
+
+#include <poutre/base/config.hpp>
+#include <poutre/structuring_element/se_interface.hpp>
+#include <poutre/structuring_element/se_chained.hpp>
+#include <cstdio>
+#include <memory>
+#include <vector>
+#include <utility>
+
+namespace poutre::se
+{
+
+void ChainedStructuringElement::append(std::unique_ptr<IStructuringElement> strel)
+{
+  m_vect_se.push_back(std::move(strel));
+}
+
+std::unique_ptr<IStructuringElement> ChainedStructuringElement::Transpose() const
+{
+  std::unique_ptr<ChainedStructuringElement> output_se = std::make_unique<ChainedStructuringElement>();
+  for(const auto & strel : m_vect_se) {
+    output_se->m_vect_se.push_back(strel->Transpose());
+  }
+  return output_se;
+}
+
+std::size_t ChainedStructuringElement::GetSize() const
+{
+  return 0;
+}
+
+std::unique_ptr<IStructuringElement> ChainedStructuringElement::RemoveCenter() const
+{
+  POUTRE_RUNTIME_ERROR("RemoveCenter not implemented for chained se");
+}
+
+std::unique_ptr<IStructuringElement> ChainedStructuringElement::Clone() const
+{
+  std::unique_ptr<ChainedStructuringElement> output_se = std::make_unique<ChainedStructuringElement>();
+  for(const auto & strel : m_vect_se) {
+    output_se->m_vect_se.push_back(strel->Clone());
+  }
+  return output_se;
+}
+
+bool ChainedStructuringElement::is_equal(const IStructuringElement& strel) const noexcept
+{
+  const auto *se_t = dynamic_cast<const ChainedStructuringElement *>(&strel);
+  if  (se_t == nullptr) {
+    return false;
+  }
+  if (se_t->m_vect_se.size() != m_vect_se.size()) {return false;}
+  for (std::size_t i = 0; i < m_vect_se.size(); ++i) {
+    if (!se_t->m_vect_se[i]->is_equal(*m_vect_se[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool ChainedStructuringElement::is_equal_unordered(const IStructuringElement& strel) const noexcept
+{
+  const auto *se_t = dynamic_cast<const ChainedStructuringElement *>(&strel);
+  if  (se_t == nullptr) {
+    return false;
+  }
+  if (se_t->m_vect_se.size() != m_vect_se.size()) {return false;}
+  for (std::size_t i = 0; i < m_vect_se.size(); ++i) {
+    if (!se_t->m_vect_se[i]->is_equal_unordered(*m_vect_se[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+}
