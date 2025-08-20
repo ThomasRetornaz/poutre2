ffd606cc9c8967bf2a73bbc93c6f5980a2eaa9b8
Thomas Retornaz <thomas.retornaz@mines-paris.org>
2025-08-20 21:31:20 +0200

    [macOS] force link to -stdlib=libc++ -lc++abi

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,17 +1,17 @@
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
-  set(CMAKE_BUILD_TYPE
-      RelWithDebInfo
-      CACHE STRING "Choose the type of build." FORCE)
-  # Set the possible values of build type for cmake-gui, ccmake
-  set_property(
-    CACHE CMAKE_BUILD_TYPE
-    PROPERTY STRINGS
-             "Debug"
-             "Release"
-             "MinSizeRel"
-             "RelWithDebInfo")
+    message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
+    set(CMAKE_BUILD_TYPE
+            RelWithDebInfo
+            CACHE STRING "Choose the type of build." FORCE)
+    # Set the possible values of build type for cmake-gui, ccmake
+    set_property(
+            CACHE CMAKE_BUILD_TYPE
+            PROPERTY STRINGS
+            "Debug"
+            "Release"
+            "MinSizeRel"
+            "RelWithDebInfo")
 endif()
 
 # Generate compile_commands.json to make it easier to work with clang based tools
@@ -19,44 +19,44 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # Enhance error reporting and compiler messages
 if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-  if(WIN32)
-    # On Windows cuda nvcc uses cl and not clang
-    add_compile_options($<$<COMPILE_LANGUAGE:C>:-fcolor-diagnostics> $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>)
-  else()
-    add_compile_options(-fcolor-diagnostics)
-  endif()
+    if(WIN32)
+        # On Windows cuda nvcc uses cl and not clang
+        add_compile_options($<$<COMPILE_LANGUAGE:C>:-fcolor-diagnostics> $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>)
+    else()
+        add_compile_options(-fcolor-diagnostics)
+    endif()
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-  if(WIN32)
-    # On Windows cuda nvcc uses cl and not gcc
-    add_compile_options($<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
-                        $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>)
-  else()
-    add_compile_options(-fdiagnostics-color=always)
-  endif()
+    if(WIN32)
+        # On Windows cuda nvcc uses cl and not gcc
+        add_compile_options($<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
+                $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>)
+    else()
+        add_compile_options(-fdiagnostics-color=always)
+    endif()
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND MSVC_VERSION GREATER 1900)
-  add_compile_options(/diagnostics:column)
-  # Setup 64bit and 64bit windows systems
-  if(CMAKE_CL_64)
-    add_definitions("-D_WIN64")
-    message(STATUS "- MSVC: 64-bit platform, enforced -D_WIN64 parameter")
+    add_compile_options(/diagnostics:column)
+    # Setup 64bit and 64bit windows systems
+    if(CMAKE_CL_64)
+        add_definitions("-D_WIN64")
+        message(STATUS "- MSVC: 64-bit platform, enforced -D_WIN64 parameter")
 
-    #Enable extended object support for all compiles on X64
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
-    message(STATUS "- MSVC: Enabled extended object-support for all-compiles")
+        #Enable extended object support for all compiles on X64
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /bigobj")
+        message(STATUS "- MSVC: Enabled extended object-support for all-compiles")
 
-  endif()
+    endif()
 else()
-  message(STATUS "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
+    message(STATUS "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
 endif()
 
 if(CMAKE_SYSTEM_NAME STREQUAL "Darwin" AND CMAKE_SYSTEM_PROCESSOR STREQUAL "arm64")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -v")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
 endif()
 
 IF (APPLE)
-  ADD_COMPILE_OPTIONS(-mmacosx-version-min=14.7) # compile option
-  set(CMAKE_OSX_DEPLOYMENT_TARGET "14.7" CACHE STRING "" FORCE) # and cmake_osx_deployment target
+    ADD_COMPILE_OPTIONS(-mmacosx-version-min=14.7) # compile option
+    set(CMAKE_OSX_DEPLOYMENT_TARGET "14.7" CACHE STRING "" FORCE) # and cmake_osx_deployment target
 ENDIF()
 
 # run vcvarsall when msvc is used
